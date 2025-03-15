331dade95e4dbebef687c7d017444b8a6804cb54
alliance74 <bienvenuealliance@gmail.com>
2025-03-16 00:32:45 +0200

    Fix path to index.html in Express route

--- backend/src/index.js
+++ backend/src/index.js
@@ -37,7 +37,7 @@ const __dirname =path.resolve();
 
 
 if(process.env.NODE_ENV==="production"){
-app.use(express.static(path.join(_dirname, "../frontend/dist")))
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
 app.get("*", (req, res)=>{
 
